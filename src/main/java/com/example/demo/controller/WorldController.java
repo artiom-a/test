24a22696ccaae7f5181a241d316d390285b84d35
Fedor Shtanko <atraxlxxxl@gmail.com>
2024-05-03 03:08:55 +0300

    ыв

--- src/main/java/com/example/demo/controller/WorldController.java
+++ src/main/java/com/example/demo/controller/WorldController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WorldController {
-    @GetMapping("/hi")
+    @GetMapping("/world")
     public String home() {
         return "Привет, мир !";
     }
