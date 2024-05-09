a0758c97759ef6bc8b4de20c5de9c2db95217cd5
ArtemA <abramyanabramyan@yandex.ru>
2024-05-09 23:05:36 +0300

    Test commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,29 @@
 package com.example.demo;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+
+		/**
+		 * Ошибка номер раз: из-за автоматической конфигурации SpringBoot приложений,
+		 * компилятор после выключения встроенного Tomcat падает в ошибку, а точнее
+		 * Exception in thread "main" java.lang.NoClassDefFoundError: org/springframework/boot/SpringApplication
+		 *
+		 */
+		//SpringApplication.run(DemoApplication.class, args);
+
+		SpringApplication app = new SpringApplication(DemoApplication.class);
+		app.setBannerMode(Banner.Mode.OFF);
+		app.setLazyInitialization(true);
+		app.setWebApplicationType(WebApplicationType.NONE); // вот эта строчка говорит в каком режиме запускать приложение
+		app.run(args);
 	}
 
 }
